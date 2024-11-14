c0b5a97c981a868ddfc740382fde2096d865f4db
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2024-11-14 01:20:48 -0800

    Summary of Sales

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -1,10 +1,44 @@
-import React from "react";
 import { useData } from "../Context/DataContext";
 
 const Summary = () => {
   const { data } = useData();
 
-  return <div>Summary</div>;
+  if (data === null) return null;
+
+  return (
+    <section>
+      <div className="summary flex">
+        <div className="box">
+          <h2>Sales</h2>
+          <span>
+            {data
+              .filter((sale) => sale.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("en-US", { style: "currency", currency: "USD" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Received</h2>
+          <span>
+            {data
+              .filter((sale) => sale.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("en-US", { style: "currency", currency: "USD" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processing</h2>
+          <span>
+            {data
+              .filter((sale) => sale.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("en-US", { style: "currency", currency: "USD" })}
+          </span>
+        </div>
+      </div>
+      <div className="box">Charts</div>
+    </section>
+  );
 };
 
 export default Summary;
